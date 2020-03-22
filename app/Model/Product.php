d7f0bd49970edef317b4aa1ac60e9572314c3f61
Dhiraj <kdhiraj3776@gmail.com>
2020-03-22 20:07:43 +0530

    "relationship created in model"

--- app/Model/Product.php
+++ app/Model/Product.php
@@ -1,10 +1,14 @@
 <?php
-
+use App\Model\Review;
 namespace App\Model;
 
 use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    //
+    //has many reviews
+    public function reviews()
+    {
+        return $this->hasMany(Review::class);
+    }
 }
--- app/Model/Review.php
+++ app/Model/Review.php
@@ -1,10 +1,14 @@
 <?php
-
+use App\Model\Product;
 namespace App\Model;
 
 use Illuminate\Database\Eloquent\Model;
 
 class Review extends Model
 {
-    //
+    //review belongs to product
+    public function product()
+    {
+        return $this->belongsTo(Product::class);
+    }
 }
